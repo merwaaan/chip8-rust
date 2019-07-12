367d73afbbe7c497c6d4ec3327b14fefa47f0c51
Merwan <achibet.merwan@gmail.com>
2019-07-12 10:03:55 +0200

    Add basic opcode decoding

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is a Chip-8 emulator written in Rust, for learning purposes.
+
+## References
+
+* [Cowgod's Chip-8 Technical Reference v1.0](http://devernay.free.fr/hacks/chip8/C8TECH10.HTM)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 fn main()
 {
+    let mut mem: [u8; 0x1000] = [0; 0x1000];
+    let mut v: [u8; 16] = [0; 16];
+    let mut i: u16 = 0;
+    let mut pc: usize = 0;
+
+
+    loop
+    {
+        let opcode = [
+            (mem[pc] & 0xF0) >> 4,
+            mem[pc] & 0x0F,
+            (mem[pc+1] & 0xF0) >> 4,
+            mem[pc+1] & 0x0F
+        ];
+
+        println!("{}", opcode[0]);
+
+        match opcode
+        {
+            [0, n1, n2, n3] => println!("SYS"),
+            [0, 0, 0xE, 0] => println!("CLS"),
+            [0, 0, 0xE, 0xE] => println!("RET"),
+            [1, n1, n2, n3] => println!("JMP"),
+            [2, n1, n2, n3] => println!("CALL"),
+            _ => panic!("unknown opcode")
+        }
+
+        pc += 2;
+    }
 }
